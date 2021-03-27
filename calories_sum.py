0446561c059c1704ac9d88c699a616bf92afc2d1
Tamara <60240971+Prussak@users.noreply.github.com>
2021-03-27 16:52:08 -0300

    Add files via upload

---
+++ calories_sum.py
@@ -0,0 +1,11 @@
+print("Today's Date:")
+today= str(input())
+print("Breakfast calories: ")
+breakfast = int(input())
+print("Lunch calories: ")
+lunch = int(input())
+print("Dinner calories: ")
+dinner = int(input())
+sum = breakfast+lunch+dinner
+
+print(today + " You consumed: " + str(sum)+ " Calories")
---
+++ numbers.py
@@ -0,0 +1,6 @@
+print("first number: ")
+first = int(input())
+print("second number")
+second = int(input())
+sum = first + second
+print(sum)
